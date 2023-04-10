195433531b6fda8a282e8189e9eca901098bad4a
Shiba <44804845+DeepDoge@users.noreply.github.com>
2023-04-10 18:42:56 +0000

    ...

--- library/index.js
+++ library/index.js
@@ -3,19 +3,17 @@ import fs from "fs"
 import { parseLiterals } from "parse-literals"
 import path from "path"
 
-const preprocessorFilename = path.join(__dirname, "./preprocess.ts")
+const preprocessorFilename = path.join("node_modules", "master-ts-vite-plugin", "library", "./preprocess.ts")
 const preprocessorTs = fs.readFileSync(preprocessorFilename, "utf8")
 const preprocessorJs = typescript.transpile(preprocessorTs, { module: "commonjs" }, preprocessorFilename)
 
-const exports = new Function("parseLiterals", `const exports = {}; ${preprocessorJs}; return exports`)(parseLiterals)
-
-console.log(exports)
+const { preprocess } = new Function("parseLiterals", `const exports = {}; ${preprocessorJs}; return exports`)(parseLiterals)
 
 export const masterTsPlugin = {
 	name: "transform-file",
 	transform(src, filename) {
 		return {
-			code: src,
+			code: preprocess(src, filename),
 		}
 	},
 }
